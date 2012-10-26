ebcd89bef0d7c6adb735d4eec4db3042b34d465d
Giovanni Intini <giovanni@mikamai.com>
2012-10-26 22:21:10 +0200

    initial commit

---
+++ lib/github-key-upload.rb
@@ -0,0 +1,9 @@
+require "github-key-upload/version"
+
+module Github
+  module Key
+    module Upload
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/github-key-upload/version.rb
@@ -0,0 +1,7 @@
+module Github
+  module Key
+    module Upload
+      VERSION = "0.0.1"
+    end
+  end
+end
