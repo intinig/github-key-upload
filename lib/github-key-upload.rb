c01f523142bf1e405e733eead0f81572d2f33c52
Giovanni Intini <giovanni@mikamai.com>
2012-10-26 23:00:24 +0200

    done

--- lib/github-key-upload.rb
+++ lib/github-key-upload.rb
@@ -1,12 +1,13 @@
 require "github-key-upload/version"
+require "github_api"
 
 module Github
   module Key
     module Upload
-      def self.upload(key, user, password)
-        puts key
-        puts user
-        puts password
+      def self.upload(options)
+        github = Github.new basic_auth: "#{options[:user]}:#{options[:password]}"
+        github.oauth.create 'scopes' => ['user']
+        github.users.keys.create({title: options[:title], key: File.read(options[:key])})
       end
     end
   end
